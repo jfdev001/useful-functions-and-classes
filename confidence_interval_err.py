bed530bcb630d92fd02855680e912d680cd12d9f
Jared Frazier <72281389+jfdev001@users.noreply.github.com>
2022-01-13 19:37:24 -0600

    Create confidence_interval_err.py

---
+++ confidence_interval_err.py
@@ -0,0 +1,31 @@
+def confidence_interval_err(vector: np.ndarray, alpha: float = 0.95):
+    """Computes desired confidence interval error."""
+
+    # Validate
+    if not(alpha > 0. and alpha < 1,):
+        raise ValueError(':param alpha: must be between 0 and 100.')
+
+    # Compute estimators
+    mean = np.mean(vector)
+    scale = st.sem(vector)  # standard error mean (how close to pop. mean)
+
+    # Determine central limit theorem assumption
+    if vector.shape[0] >= 30:
+        print('Assume CLT...')
+        interval = st.norm.interval(alpha=alpha, loc=mean, scale=scale)
+
+    else:
+        print('Not assuming CLT...')
+        interval = st.t.interval(alpha=alpha, df=len(
+            vector)-1, loc=mean, scale=scale)
+
+    # Confidence intervals are m +- h
+    # from left_bound = mean - h and right_bound = mean + h
+    # From the 'Example' at https://en.wikipedia.org/wiki/Confidence_interval
+    # it is clear that [mean - cs/sqrt(n), mean + cs/sqrt(n)] means that the
+    # value of cs/sqrt(n), denoted err can be computed with simple rearrangement.
+    left_bound, right_bound = interval
+    err = right_bound - mean
+
+    # Resulting err for errorbars
+    return err
